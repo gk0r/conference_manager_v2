f4094ac5a51a02d8a44be5d3deadef67312b8fa0
gk0r <gk0r.go@gmail.com>
2012-08-23 15:38:23 +1000

    Fixed the attr_accessible in Conference Number

--- app/models/conference_number.rb
+++ app/models/conference_number.rb
@@ -2,6 +2,8 @@ class ConferenceNumber < ActiveRecord::Base
   
   has_many :bookings, :dependent => :destroy
   audited
+  
+  attr_accessible :conference_number
 
   def to_label 
     "#{conference_number}" 
